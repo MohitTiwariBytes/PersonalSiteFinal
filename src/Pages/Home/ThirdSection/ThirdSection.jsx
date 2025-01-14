05d40ed43a00305d4f82ca5e44f5959589b1d108
Mohit Tiwari <mohittiwaridev@gmail.com>
2025-01-14 19:02:34 +0530

    image scroll parallax added

--- src/Pages/Home/ThirdSection/ThirdSection.jsx
+++ src/Pages/Home/ThirdSection/ThirdSection.jsx
@@ -8,12 +8,59 @@ import Button from "../../../Components/ButtonAnimated/Button";
 gsap.registerPlugin(ScrollTrigger);
 
 export default function ThirdSection() {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const imageInsiders = containerRef.current.querySelectorAll('.asdais-inner');
+
+    imageInsiders.forEach((insider) => {
+      gsap.fromTo(insider,
+        {
+          transform: "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, -100, 0, 1)"
+        },
+        {
+          transform: "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 100, 0, 1)",
+          scrollTrigger: {
+            trigger: insider.parentElement,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true,
+          }
+        }
+      );
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    };
+  }, []);
+
+
+
   const paragraphsRef = useRef([]);
   const imageRef = useRef(null);
 
   useEffect(() => {
     // Safely select references
     const paragraphs = paragraphsRef.current;
+
+
+    gsap.fromTo(
+      ".button",
+      { y: 20, opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 0.5,
+        ease: "power2.out",
+        stagger: 0.2,
+        scrollTrigger: {
+          trigger: ".button",
+          start: "top 70%",
+          toggleActions: "play none none reverse",
+        },
+      }
+    )
 
 
     // Paragraphs animation
@@ -45,7 +92,7 @@ export default function ThirdSection() {
   }, []);
 
   return (
-    <div className="main-third-section">
+    <div className="main-third-section" ref={containerRef}>
       <div className="thirdSection">
         <div className="about">
           <div className="left1">
@@ -72,12 +119,7 @@ export default function ThirdSection() {
           </div>
           <div className="image">
             <div className="asdais">
-              <img
-                id="imaads"
-                ref={imageRef}
-                src={myImage}
-                alt="Profile"
-              />
+              <div className="asdais-inner"></div>
             </div>
           </div>
         </div>
